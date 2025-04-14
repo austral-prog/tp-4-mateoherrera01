56d84b4e18bb56e44579d1f2147feff56cbf0403
mateoherrera01 <maherrera@mail.austral.edu.ar>
2025-04-14 14:42:29 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
     print("TO DO")
+    año=int(input("Ingrese un año: "))
+    
+    if (año%4==0 or año%400==0):
+        print("El año " + str(año) + " es bisiesto")
+    else: 
+        print("El año " + str(año) + " no es bisiesto")
